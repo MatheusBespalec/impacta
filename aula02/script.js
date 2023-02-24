10c946a02a5021235942f5e09e488ef8cab3c61c
Matheus Bespalec <matheusbespalec@gmail.com>
2023-02-23 22:14:11 -0300

    feature: adicionando validação no formulario

--- aula02/script.js
+++ aula02/script.js
@@ -1,10 +1,19 @@
 const form = document.getElementById('form');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    const name = document.getElementById('name');
-    const phone = document.getElementById('phone');
-    const email = document.getElementById('email');
-    const message = document.getElementById('message');
+    data = {
+        'name': document.getElementById('name').value,
+        'phone': document.getElementById('phone').value,
+        'email': document.getElementById('email').value,
+        'message': document.getElementById('message').value,
+    }
 
-    alert(`Obrigado Sr(a). ${name.value} os seus dados foram encaminhados com sucesso! Estes foram os dados enviados: Nome: ${name.value} | Telefone: ${phone.value} | E-mail: ${email.value} | Mensagem: ${message.value}`);
+    for (let input in data) {
+        if (data[input] == '') {
+            alert('Preencha todos os campos!');
+            return;
+        }
+    }
+
+    alert(`Obrigado Sr(a). ${data.name} os seus dados foram encaminhados com sucesso! Estes foram os dados enviados: Nome: ${data.name} | Telefone: ${data.phone} | E-mail: ${data.email} | Mensagem: ${data.message}`);
 });
