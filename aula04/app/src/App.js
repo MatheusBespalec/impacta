495a17db8e301d0e22ee7ce2724b8f7aebf5aa2a
Matheus Bespalec <matheusbespalec@gmail.com>
2023-04-19 00:09:43 -0300

    feat: ac2

--- aula04/app/src/App.js
+++ aula04/app/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Route, Routes, Link} from 'react-router-dom';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
 
 function App() {
   return (
-    <div className="App">
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Frameworks FullStack
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <p><Link to='/page-1'>Página 1</Link></p>
+        <p><Link to='/page-2'>Página 2</Link></p>
       </header>
+      <main>
+          <Routes>
+            <Route path='/page-1' element= {<Page1/>}/>
+            <Route path='/page-2' element= {<Page2/>}/>
+          </Routes>
+        </main>
     </div>
   );
 }
